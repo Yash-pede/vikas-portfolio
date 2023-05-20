5de785390f4d8df231af7d8a3559e4b1c23b5537
Yash-pede <yashpede@ssipmt.com>
2023-05-20 22:53:55 +0530

    add projects test

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,31 +111,5 @@ export const projects = [
     project_link:
       "https://drive.google.com/file/d/1SJIxvg3gWLVW1rG1DGLJ15xr8dRGCXeh/view?usp=share_link",
   },
-  {
-    name: "JARVIS",
-    description:
-       "Jarvis is a virtual assistant that I made with Python and some abracadabra. It can do anything from switching lights to texting bae to playing jams to googling stuff. Jarvis is like the Marvel dude who cracks jokes with Iron Man. Jarvis is not just a voice assistant, but a BFF that makes me LOL.😂😎👌",
-    tags: [
-      {
-        name: "Python",
-        color: "blue",
-      },
-      {
-        name: "Speech",
-        color: "green ",
-      },
-      {
-        name: "Awkwardly awesome",
-        color: "pink",
-      },
-      {
-        name: "The video will be updates soon so Stay tuned",
-        color: "red",
-      },
-    ],
-    image: images.embc,
-    source_code_link: "https://github.com/Yash-pede/AI_YASH",
-    project_link:
-      "https://drive.google.com/file/d/1SJIxvg3gWLVW1rG1DGLJ15xr8dRGCXeh/view?usp=share_link",
-  },
+ 
 ];
